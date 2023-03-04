c52fc6346af4eb4b3abeb2bee19eb7d8bcd0555e
donib-irakihda <donib.irakihda@gmail.com>
2023-03-05 01:33:53 +0545

    added pagination

--- web.py
+++ web.py
@@ -2,34 +2,49 @@ from bs4 import BeautifulSoup
 import requests
 
 root = 'https://subslikescript.com'
-website = f'{root}/movies'
+website = f'{root}/movies_letter-A'
 result = requests.get(website)
 content = result.text
-
 soup = BeautifulSoup(content, 'lxml')
 # print(soup.prettify())
 
-box = soup.find('article', class_='main-article')
+# Pagination
+pagination = soup.find('ul', class_='pagination')
+pages = pagination.find_all('li', class_='page-item')
+last_page = pages[-2].text
 
 links = []
-for link in box.find_all('a', href=True):
-    links.append(link['href'])
-
-print(links)
-
-for link in links:
-    website = f'{root}/{link}'
-    result = requests.get(website)
+for page in range(1, int(last_page)+1)[:2]:
+    result = requests.get(f'{website}?page={page}')
     content = result.text
     soup = BeautifulSoup(content, 'lxml')
-    
+
     box = soup.find('article', class_='main-article')
 
-    title = box.find('h1').get_text()
-    transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+    
+    for link in box.find_all('a', href=True):
+        links.append(link['href'])
 
-    with open(f'{title}.txt', 'w') as file:
-        file.write(transcript)
+    print(links)
+
+    for link in links:
+        try:
+            print(link)
+            result = requests.get(f'{root}/{link}')
+            content = result.text
+            soup = BeautifulSoup(content, 'lxml')
+            
+            box = soup.find('article', class_='main-article')
+
+            title = box.find('h1').get_text()
+            transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+            with open(f'{title}.txt', 'w') as file:
+                file.write(transcript)
+        except:
+            print('Link not working' + link)
+
+    
 
 
 
