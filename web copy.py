00bef4d889f9e7688cf1b431029aabb8ab942783
donib-irakihda <donib.irakihda@gmail.com>
2023-03-05 00:39:06 +0545

    added few scraped data and few script updates

---
+++ web copy.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+website = 'https://subslikescript.com/movie/Titanic-120338'
+result = requests.get(website)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+# print(soup.prettify())
+
+
+box = soup.find('article', class_='main-article')
+title = box.find('h1').get_text()
+
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+with open(f'{title}.txt', 'w') as file:
+    file.write(transcript)
--- web.py
+++ web.py
@@ -1,18 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movie/Titanic-120338'
+root = 'https://subslikescript.com'
+website = f'{root}/movies'
 result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
 # print(soup.prettify())
 
-
 box = soup.find('article', class_='main-article')
-title = box.find('h1').get_text()
 
-transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+links = []
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
 
-with open(f'{title}.txt', 'w') as file:
-    file.write(transcript)
+print(links)
+
+for link in links:
+    website = f'{root}/{link}'
+    result = requests.get(website)
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+    
+    box = soup.find('article', class_='main-article')
+
+    title = box.find('h1').get_text()
+    transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+    with open(f'{title}.txt', 'w') as file:
+        file.write(transcript)
+
+
+
